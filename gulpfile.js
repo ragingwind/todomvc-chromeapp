e01dd9f201529ac6e1f589cec83c9c3d716f6ed0
ragingwind <ragingwind@gmail.com>
2015-01-12 11:35:45 +0900

    Update bundle task

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,21 @@ var vulcanize = require('gulp-vulcanize');
 var polymports = require('gulp-polymports');
 
 var configs = {
-  elements: 'app/elements/'
+  bundles: 'app/elements/bundles'
 };
 
-gulp.task('components', function() {
-  return polymports.src(require('./components.json'))
-  .pipe(vulcanize({
-    dest: path.join(__dirname, configs.elements),
-    csp: true,
-    excludes: {
-      imports: [
-        "polymer.html$"
-      ]
-    }
-  }))
-  .pipe(gulp.dest(configs.elements));
+// Make elements bundles that can be elements included frequency, polymer sets
+// or set of static elements for yours
+gulp.task('vulcanize:bundles', function() {
+  return polymports.src(require('./vulcanize.json').bundles)
+    .pipe(vulcanize({
+      dest: path.join(__dirname, configs.bundles),
+      csp: true,
+      inline: true
+    }))
+    .pipe(gulp.dest(configs.bundles));
+});
+
+// Build your Chrome App
+gulp.task('build', ['vulcanize:bundles'], function() {
 });
